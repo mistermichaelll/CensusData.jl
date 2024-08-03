7f17af51a476e57c8b25afd4a932a73635c72d79
Michael Johnson <mistermichaellll@gmail.com>
2024-08-03 17:25:13 -0400

    add a test for DC

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,20 @@
 using CensusData
+using DataFrames: nrow, ncol
 using Test
 
-@testset "CensusData.jl" begin
-    # Write your tests here.
-end
+@testset "results of get_acs_data() make sense for DC" begin
+        dc = get_acs_data(
+            year = 2021,
+            survey = "acs5",
+            vars = [
+                "B01001_001E", # total population
+                "B08006_001E", # total commuters
+                "B08006_014E", # bike commuters
+                "B08006_017E", # worked from home
+            ],
+            _for = "place",
+            _in = "state:11"
+        )
+        @test ncol(a) == 7 # expect 7 columns (4 variables + the name [included by default], place, and state).
+        @test nrow(a) == 1 # there should only be one row for DC's place data.
+end;
