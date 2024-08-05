fb83c5a46cb2cf9d4ef322d4187285482c4a6084
Michael Johnson <mistermichaellll@gmail.com>
2024-08-05 11:58:09 -0400

    add more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using CensusData
 using DataFrames: nrow, ncol
 using Test
 
-@testset "results of get_acs_data() make sense for DC" begin
+@testset "results of get_acs_data() make sense" begin
         dc = get_acs_data(
             year = 2021,
             survey = "acs5",
@@ -15,6 +15,25 @@ using Test
             _for = "place",
             _in = "state:11"
         )
-        @test ncol(a) == 7 # expect 7 columns (4 variables + the name [included by default], place, and state).
-        @test nrow(a) == 1 # there should only be one row for DC's place data.
+        @test ncol(dc) == 7 # expect 7 columns (4 variables + the name [included by default], place, and state).
+        @test nrow(dc) == 1 # there should only be one row for DC's place data.
 end;
+
+@testset "trying to get acs1 for 2020 throws an error" begin
+    @test_throws "The regular 1-year ACS for 2020 was not released and is not available in CensusData.jl." get_acs_data(
+        year = 2020,
+        survey = "acs1",
+        vars = [
+            "B01001_001E", # total population
+            "B08006_001E", # total commuters
+            "B08006_014E", # bike commuters
+            "B08006_017E", # worked from home
+        ],
+        _for = "place",
+        _in = "state:11"
+    )
+end;
+
+# @testset "ensure columns make sense" begin
+#     #TODO: need to make sure columns that are returned have a sensible type.
+# end;
